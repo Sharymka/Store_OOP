4e8c0409c34200d2b115a236050fd5cf8a25312b
Svetlana <sveta.sharymova@gmail.com>
2024-02-28 14:49:23 +0400

    01:create_dir

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname)); // Раздача файлов из текущей директории
 
-app.listen(3100, () => {
+app.listen(3200, () => {
   console.log(
     'Сервер запущен на порту 3100! Перейти: http://localhost:3100/store.html'
   );
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import { ProductsList } from './Products/ProducstList';
+import { BasketList } from './Basket/BasketList.js';
+
+
+const basketList = new BasketList();
+new ProductsList(basketList);
+
+
+
+
+
